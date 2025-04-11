220993b32598d8f226ca0e07d09abb8a393e9b86
hgallo0 <gallohenry@gmail.com>
2025-04-11 09:53:14 -0400

    removed redundant tags

--- autoscaling_group.tf
+++ autoscaling_group.tf
@@ -3,12 +3,10 @@ resource "aws_autoscaling_group" "bastion" {
   max_size                  = var.max_size
   min_size                  = var.min_size
   health_check_grace_period = var.gracePeriod
-  #health_check_type         = "EC2"
   desired_capacity     = var.desired_capacity
   force_delete         = true
   launch_configuration = aws_launch_configuration.as_conf.name
   vpc_zone_identifier  = var.subnet_id
-  #availability_zones   = var.azs
 
   dynamic "tag" {
     for_each = var.asg_tags
@@ -23,10 +21,5 @@ resource "aws_autoscaling_group" "bastion" {
     delete = "15m"
   }
 
-  tag {
-    key                 = "Class"
-    value               = "cs622"
-    propagate_at_launch = true
-  }
   depends_on = [aws_launch_configuration.as_conf]
 }
