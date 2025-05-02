2b8236f79e62d4b60f2475b4bf958b0945fad281
hgallo0 <gallohenry@gmail.com>
2025-05-01 20:37:05 -0400

    updated sg name

--- launch_config.tf
+++ launch_config.tf
@@ -7,7 +7,7 @@ resource "aws_launch_configuration" "as_conf" {
   instance_type        = var.instance
   user_data            = data.template_file.default.rendered
   key_name             = var.ssh_key
-  security_groups      = [aws_security_group.allow_ssh.id]
+  security_groups      = [aws_security_group.allow_traffic.id]
   iam_instance_profile = var.bastion_role
   lifecycle {
     create_before_destroy = true
